b048eb2fe45e80eba9f0de605268809caa3686b9
Ihor <horizont2060.gmail.com>
2022-09-15 14:04:55 +0300

    modal

--- js/script.js
+++ js/script.js
@@ -1,106 +1,136 @@
-window.addEventListener('DOMContentLoaded', () => {
-   
-    //​‌‌‍⁡⁢⁣⁢Tabs⁡​
+window.addEventListener("DOMContentLoaded", () => {
+  //​‌‌‍⁡⁢⁣⁢Tabs⁡​
 
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabParents = document.querySelector('.tabheader__items');
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabParents = document.querySelector(".tabheader__items");
 
-    function hideTabContent() {
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show', 'fade');
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    hideTabContent();
-    showTabContent();
-
-    tabParents.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if (target && target.classList.contains('tabheader__item')) {
-            tabs.forEach((item, i) => {
-                if (target == item) {
-                    hideTabContent();
-                    showTabContent(i);
-                }
-            });
-        }
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
     });
 
-    //​‌‌‍⁡⁢⁣⁢Timer⁡​
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
 
-    const deadline = '2022-09-23';
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
 
-    function getTimeRemaining(endtime) {
+  hideTabContent();
+  showTabContent();
 
-        let days, hours, minutes, seconds;
-        const t = Date.parse(endtime) - Date.parse(new Date()); //отнимает от текущей даты в миллисекундах
+  tabParents.addEventListener("click", (event) => {
+    const target = event.target;
 
-        if (t <= 0) {
-            days = 0,
-            hours = 0,
-            minutes = 0,
-            seconds = 0;
-        } else {
-            days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60)) % 24), // получаем количество часов,которых не хватает до полных суток(остаток), потому что что может вывести 50 часов
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
         }
+      });
+    }
+  });
 
+  //​‌‌‍⁡⁢⁣⁢Timer⁡​
 
-        return {
-            'total': t,
-            'days': days,
-            'hours': hours,
-            'minutes': minutes,
-            'seconds': seconds
-        };
+  const deadline = "2022-09-23";
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date()); //отнимает от текущей даты в миллисекундах
+
+    if (t <= 0) {
+      (days = 0), (hours = 0), (minutes = 0), (seconds = 0);
+    } else {
+      (days = Math.floor(t / (1000 * 60 * 60 * 24))),
+        (hours = Math.floor((t / (1000 * 60 * 60)) % 24)), // получаем количество часов,которых не хватает до полных суток(остаток), потому что что может вывести 50 часов
+        (minutes = Math.floor((t / (1000 * 60)) % 60)),
+        (seconds = Math.floor((t / 1000) % 60));
     }
 
-    function getZero(num) {  // добавляет ноль к числу, если оно меньше 10
-        if (num >= 0 && num < 10) {
-            return `0${num}`;
-        } else {
-            return num;
-        }
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    // добавляет ноль к числу, если оно меньше 10
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
     }
+  }
 
-    function setClock(selector, endtime) {
-        const timer = document.querySelector(selector),
-              days = timer.querySelector('#days'),
-              hours = timer.querySelector('#hours'),
-              minutes = timer.querySelector('#minutes'),
-              seconds = timer.querySelector('#seconds'),
-              timeInterval = setInterval(updateClock, 1000); //запускается каждую секунду
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000); //запускается каждую секунду
 
-        updateClock(); //запускается сразу, чтоб убрать мигание таймера(изначально там значения с вёрстки)
+    updateClock(); //запускается сразу, чтоб убрать мигание таймера(изначально там значения с вёрстки)
 
-        function updateClock() {
-            const t = getTimeRemaining(endtime);
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
 
-            days.innerHTML = getZero(t.days);
-            hours.innerHTML = getZero(t.hours);
-            minutes.innerHTML = getZero(t.minutes);
-            seconds.innerHTML = getZero(t.seconds);
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
 
-            if (t.total <= 0) {
-                clearInterval(timeInterval);
-            }
-        }
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    } 
+  }
+  setClock('.timer', deadline);
+
+  //​‌‌‍⁡⁢⁣⁢Modal window⁡​
+
+  const modalTrigger = document.querySelectorAll("[data-modal]"),
+        modal = document.querySelector(".modal"),
+        modalCloseBtn = document.querySelector("[data-close]");
+
+  modalTrigger.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      modal.classList.add("show");
+      modal.classList.remove("hide");
+      // modal.classList.toggle('show'); можно использовать вместо двух верхних
+      document.body.style.overflow = "hidden"; //при появлении окна убирает прокрутку страницы
+    });
+  });
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow = ""; // возврвщает прокрутку на странице
+  }
+
+  modalCloseBtn.addEventListener("click", closeModal);
+
+
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
     }
+  });
 
-    setClock('.timer', deadline);
+  document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) { // привязывает закрытие к Esc и будет работать только тогда, когда открыто модальное окно
+        closeModal();
+    }
+  });
 });
