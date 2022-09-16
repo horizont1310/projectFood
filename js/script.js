0ca35722511c09af36ec2d9eb7f81c5c8ae53794
Ihor <horizont2060.gmail.com>
2022-09-16 11:18:42 +0300

    UpdateModalWindow

--- js/script.js
+++ js/script.js
@@ -94,43 +94,59 @@ window.addEventListener("DOMContentLoaded", () => {
       if (t.total <= 0) {
         clearInterval(timeInterval);
       }
-    } 
+    }
   }
-  setClock('.timer', deadline);
+  setClock(".timer", deadline);
 
   //​‌‌‍⁡⁢⁣⁢Modal window⁡​
 
   const modalTrigger = document.querySelectorAll("[data-modal]"),
-        modal = document.querySelector(".modal"),
-        modalCloseBtn = document.querySelector("[data-close]");
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    // modal.classList.toggle('show'); можно использовать вместо двух верхних
+    document.body.style.overflow = "hidden"; //при появлении окна убирает прокрутку страницы
+  }
 
   modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modal.classList.add("show");
-      modal.classList.remove("hide");
-      // modal.classList.toggle('show'); можно использовать вместо двух верхних
-      document.body.style.overflow = "hidden"; //при появлении окна убирает прокрутку страницы
-    });
+    btn.addEventListener("click", openModal);
   });
 
   function closeModal() {
     modal.classList.add("hide");
     modal.classList.remove("show");
     document.body.style.overflow = ""; // возврвщает прокрутку на странице
+    clearInterval(modalTimerId);
   }
 
   modalCloseBtn.addEventListener("click", closeModal);
 
-
-  modal.addEventListener('click', (e) => {
+  modal.addEventListener("click", (e) => {
     if (e.target === modal) {
-        closeModal();
+      closeModal();
     }
   });
 
-  document.addEventListener('keydown', (e) => {
-    if (e.code === "Escape" && modal.classList.contains('show')) { // привязывает закрытие к Esc и будет работать только тогда, когда открыто модальное окно
-        closeModal();
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      // привязывает закрытие к Esc и будет работать только тогда, когда открыто модальное окно
+      closeModal();
     }
   });
+
+  const modalTimerId = setTimeout(openModal, 30000);
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { //1 - прокрутка всего окна 2 - видимая часть у пользователя 3 - полная высота сайта
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll); //после того, как сделает один раз - удалит обработчик событий(⁡⁢⁣⁢нужно укзывать событие и функцию!!!!!!!!⁡)
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
+
 });
