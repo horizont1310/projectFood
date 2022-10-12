f123613f28ce0a4a21ff4bb095a80fb630ce1552
Ihor <horizont2060.gmail.com>
2022-10-12 13:24:41 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -101,8 +101,8 @@ window.addEventListener("DOMContentLoaded", () => {
   //​‌‌‍⁡⁢⁣⁢Modal window⁡​
 
   const modalTrigger = document.querySelectorAll("[data-modal]"),
-    modal = document.querySelector(".modal"),
-    modalCloseBtn = document.querySelector("[data-close]");
+    modal = document.querySelector(".modal");
+  // modalCloseBtn = document.querySelector("[data-close]");
 
   function openModal() {
     modal.classList.add("show");
@@ -122,10 +122,11 @@ window.addEventListener("DOMContentLoaded", () => {
     clearInterval(modalTimerId);
   }
 
-  modalCloseBtn.addEventListener("click", closeModal);
+  // modalCloseBtn.addEventListener("click", closeModal);
 
   modal.addEventListener("click", (e) => {
-    if (e.target === modal) {
+    if (e.target === modal || e.target.getAttribute("data-close") == "") {
+      // второй таргет добавился для того, чтоб можно было влиять на динамическое появление крестика(если он будет создаваться через js)
       closeModal();
     }
   });
@@ -137,16 +138,220 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  const modalTimerId = setTimeout(openModal, 30000);
+  const modalTimerId = setTimeout(openModal, 50000); //⁡⁢⁣⁢скрипт с модальным окном, в конце⁡ ⁡⁢⁣⁢нужно ​‌‌‍раскоментировать​⁡
 
   function showModalByScroll() {
-    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) { //1 - прокрутка всего окна 2 - видимая часть у пользователя 3 - полная высота сайта
+    if (
+      window.pageYOffset + document.documentElement.clientHeight >=
+      document.documentElement.scrollHeight
+    ) {
+      //1 - прокрутка всего окна 2 - видимая часть у пользователя 3 - полная высота сайта
       openModal();
-      window.removeEventListener('scroll', showModalByScroll); //после того, как сделает один раз - удалит обработчик событий(⁡⁢⁣⁢нужно укзывать событие и функцию!!!!!!!!⁡)
+      window.removeEventListener("scroll", showModalByScroll); //после того, как сделает один раз - удалит обработчик событий(⁡⁢⁣⁢нужно укзывать событие и функцию!!!!!!!!⁡)
     }
   }
 
-  window.addEventListener('scroll', showModalByScroll);
+  window.addEventListener("scroll", showModalByScroll);
 
+  // ​‌‌‍⁡⁢⁣⁢Используем классы для карточек⁡​
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.classes = classes;
+      this.parentSelector = document.querySelector(parentSelector);
+      this.transfer = 36;
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement("div");
+
+      if (this.classes.length === 0) {
+        //⁡⁢⁣⁢срабатывает ели не задан класс⁡
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+
+      element.innerHTML = `
+        <img src=${this.src} alt=${this.alt}>
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+        </div>
+      `;
+      this.parentSelector.append(element);
+    }
+  }
+
+  const getResource = async (url) => {
+    const res = await fetch(url);
+
+    if(!res.ok) {
+      throw new Error(`Could not fatcg ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  };
+
+  // getResource('http://localhost:3000/menu') //создание карточек через сервер
+  //       .then(data => {
+          // data.forEach(({img, altimg, title, descr, price}) => { //это деструктуризация( объекты в db.json)
+          //   new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+          // });
+  //       });
+
+  axios.get('http://localhost:3000/menu')
+    .then(data => {
+      data.data.forEach(({img, altimg, title, descr, price}) => { // первая data - это переменная, а вторая - именно данные из того, что приходит от сервера
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
+    });
+
+  // ⁡⁢⁣⁢​‌‌‌Forms​⁡
+
+  const forms = document.querySelectorAll("form");
+
+  const message = {
+    loading: "img/form/spinner.svg",
+    success: "Спасибо! Мы скоро перезвоним Вам",
+    failur: "Что-то пошло не так",
+  };
+
+  forms.forEach((item) => {
+    bindPostData(item);
+  });
+
+  const postData = async (url, data) => {
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: data
+    });
+
+    return await res.json();
+  };
+
+  function bindPostData(form) {
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement("img");
+      statusMessage.src = message.loading;
+      statusMessage.style.cssText = `
+          display: block;
+          margin: 0 auto;
+        `;
+
+      form.insertAdjacentElement("afterend", statusMessage); //вместо append 1 - куда вставлять элемент 2 -что вставить
+
+      const formData = new FormData(form);
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+      postData("http://localhost:3000/requests", json)
+      .then((data) => {
+        console.log(data);
+        showThanksModal(message.success);        
+        statusMessage.remove();
+      }).catch(() => {
+        showThanksModal(message.failur);
+      }).finally(() => {
+        form.reset();
+      });
+    });
+  }
+
+  function showThanksModal(message) {
+    const prevModalDialog = document.querySelector(".modal__dialog");
+
+    prevModalDialog.classList.add("hide");
+    openModal();
+
+    const thanksModal = document.createElement("div");
+    thanksModal.classList.add("modal__dialog");
+    thanksModal.innerHTML = `
+        <div class="modal__content">
+          <div class="modal__close" data-close>×</div>
+          <div class="modal__title">${message}</div>
+        </div>
+      `;
+
+    document.querySelector(".modal").append(thanksModal);
+    setTimeout(() => {
+      thanksModal.remove();
+      prevModalDialog.classList.add("show");
+      prevModalDialog.classList.remove("hide");
+      closeModal();
+    }, 4000);
+  }
+
+  fetch('http://localhost:3000/menu') // в терминале(если установлен локально json-server) npx json-server --watch db.json
+    .then(data => data.json())
+    // .then(res => console.log(res));
+
+// ⁡⁢⁣⁢​‌‌‌Slider​⁡
+
+const slides = document.querySelectorAll('.offer__slide'),
+      prev = document.querySelector('.offer__slider-prev'),
+      next = document.querySelector('.offer__slider-next'),
+      total = document.querySelector('#total'),
+      current = document.querySelector('#current');
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+if (slides.length < 10) {                    //Общее количество слайдов(отображается)
+  total.textContent = `0${slides.length}`;
+} else {
+  total.textContent = slides.length;
+}
+
+function showSlides(n) {
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  slides.forEach(item => item.style.display = 'none');
+
+  slides[slideIndex - 1].style.display = 'block';
+
+  if (slides.length < 10) {
+    current.textContent = `0${slideIndex}`;
+  } else {
+    current.textContent = slideIndex;
+  }
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+prev.addEventListener('click', () => {
+  plusSlides(-1);
+});
+
+next.addEventListener('click', () => {
+  plusSlides(1);
+});
 
 });
+
