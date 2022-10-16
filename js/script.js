bd06a1e2e49f9bdf8dd89f71e41b0a118c80d7d1
Ihor <horizont2060.gmail.com>
2022-10-16 14:44:57 +0300

    Slider regular expresion

--- js/script.js
+++ js/script.js
@@ -408,11 +408,15 @@ for (let i = 0; i < slides.length; i++) { // создание точек
     slidesField.style.transform = `translateX(-${offset}px)`;
   }
 
+  function deleteNotDigits(str) {
+    return +str.replace(/\D/g, '');
+  }
+
   next.addEventListener("click", () => {
-    if (offset == parseInt(width) * (slides.length - 1)) {
+    if (offset == deleteNotDigits(width) * (slides.length - 1)) {
       offset = 0;
     } else {
-      offset += parseInt(width);
+      offset += deleteNotDigits(width);
     }
 
     if (slideIndex == slides.length) {
@@ -428,9 +432,9 @@ for (let i = 0; i < slides.length; i++) { // создание точек
 
   prev.addEventListener("click", () => {
     if (offset == 0) {
-      offset = parseInt(width) * (slides.length - 1);
+      offset = deleteNotDigits(width) * (slides.length - 1);
     } else {
-      offset -= parseInt(width);
+      offset -= deleteNotDigits(width);
     }
 
     if (slideIndex == 1) {
@@ -449,7 +453,7 @@ for (let i = 0; i < slides.length; i++) { // создание точек
       const slideTo = e.target.getAttribute('data-slide-to');
 
       slideIndex = slideTo;
-      offset = parseInt(width) * (slideTo - 1);
+      offset = deleteNotDigits(width) * (slideTo - 1);
 
       slidesTransform();
       sliderLengthLessTen();
